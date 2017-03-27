af9e6e65f91ba264db86ed74e0bf57a1c3800b3a
palaogn <palaogn@gmail.com>
2017-03-27 21:01:53 +0000

    apparently the pipe class didn't come along

--- app/scripts/pipe.js
+++ app/scripts/pipe.js
@@ -3,11 +3,16 @@ window.Pipe = (function() {
 
   // All these constants are in em's, multiply by 10 pixels
   // for 1024x576px canvas.
+	// Pipe hight is 35 and gap between is 15
   var SPEED = 30; // * 10 pixels per second
   var WIDTH = 12.2;
   var HEIGHT = 100.6;
   var INITIAL_POSITION_X = 102.4;
-  var INITIAL_POSITION_Y = -30;
+  var INITIAL_POSITION_Y_1 = -30;
+	var INITIAL_POSITION_Y_2 = -15;
+	var INITIAL_POSITION_Y_3 = -20;
+	var INITIAL_POSITION_Y_4 = -35;
+	var INITIAL_POSITION_Y_4 = -25;
 
   var Pipe = function(el, game) {
   	this.el = el;
@@ -18,7 +23,7 @@ window.Pipe = (function() {
   //Resets the state of the pipe for a new game
   Pipe.prototype.reset = function() {
   	this.pos.x = INITIAL_POSITION_X;
-  	this.pos.y = INITIAL_POSITION_Y;
+  	this.pos.y = INITIAL_POSITION_Y_2;
   };
 
   Pipe.prototype.onFrame = function(delta) {
