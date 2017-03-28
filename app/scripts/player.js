9001e58cc046d6f6eb239719a347991f9bf08a6d
palaogn <palaogn@gmail.com>
2017-03-28 18:47:15 +0000

    removed unessasarly lines that were commented out

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -37,19 +37,6 @@ window.Player = (function() {
 	};
 
 	Player.prototype.onFrame = function(delta) {
-		/*
-		if (Controls.keys.right) {
-			this.pos.x += delta * SPEED;
-		}
-		if (Controls.keys.left) {
-			this.pos.x -= delta * SPEED;
-		}
-		if (Controls.keys.down) {
-			this.pos.y += delta * SPEED;
-		}
-		if (Controls.keys.up) {
-			this.pos.y -= delta * SPEED;
-		} */
 		if (Controls.keys.space) {
 			document.getElementById('jump_sound').play();
 			this.jump();
